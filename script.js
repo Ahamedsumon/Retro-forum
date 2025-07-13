06e02a4d57324024296d75a17ccf17fcdcab3d36
Ahamed Sumon <ahamedsumon13579@gmail.com>
2025-07-13 15:48:13 +0300

    added latest post card dynamically

--- script.js
+++ script.js
@@ -21,11 +21,14 @@ const AllPosts = async() => {
     const data = await res.json();
     const posts = data.posts;
     displayData(posts)
+    
+    
 }
 
 
 
 const displayData = (posts) => {
+  
     posts.forEach(post => {
     // const authorImage = document.getElementById('author-image');
     // authorImage.innerHTML = `
@@ -47,19 +50,18 @@ const displayData = (posts) => {
     // const postedTime = document.getElementById('posted-time');
     // postedTime.innerText = post.posted_time;
     console.log(post)
-
     const cardContainer = document.getElementById('card-container');
     
 
     const cardText = document.createElement('div');
 
     cardText.innerHTML = `
-    <div class="bg-[#797DFC1A] border border-[#797DFCFF] rounded-2xl p-5 flex gap-3">
+    <div class="bg-[#797DFC1A] border border-[#797DFCFF] rounded-2xl p-5 flex gap-3 mb-3">
             <div class="flex">
               <div id="author-image" class="h-20 w-20 bg-white rounded-lg">
                 
               </div>
-              <div id="isActiveDot"></div>
+              <div id="isActiveDot" class="h-4 w-4 rounded-full"></div>
             </div>
             <div>
               <div class="flex gap-5 mb-1">
@@ -78,7 +80,7 @@ const displayData = (posts) => {
                 <p><span id="posted-time">${post.posted_time}</span> min</p>
               </div>
               <div>
-                <button id="envelop"><i class="fa-regular fa-envelope-open p-2 bg-[#10B981FF] text-white rounded-full "></i></button>
+                <button onclick="buttonHandler()"><i class="fa-regular fa-envelope-open p-2 bg-[#10B981FF] text-white rounded-full "></i></button>
               </div>
               </div>
             </div>
@@ -89,14 +91,7 @@ const displayData = (posts) => {
 
     isActiveCheck(post.isActive)
 
-    const handleEnvelopeButton = document.getElementById('envelop');
-handleEnvelopeButton.addEventListener("click", function(){
-    const markAsRead = document.getElementById('mark-as-read');
-    markAsRead.innerHTML = `
-        <p class="max-w-64">${post.title}</p>
-              <p><i class="fa-regular fa-eye cursor-pointer"></i><span>${post.view_count}</span></p>
-    `
-})
+
 
 
 
@@ -106,20 +101,98 @@ handleEnvelopeButton.addEventListener("click", function(){
 
 
 const isActiveCheck = (isActive) => {
-    if(isActive === false){
+    if(isActive === true){
         const isActiveDot = document.getElementById('isActiveDot');
-        isActiveDot.classList = ` h-4 w-4 rounded-full bg-[#10B981FF]`
+        isActiveDot.classList.add(`bg-[#10B981FF]`)
+        // isActiveDot.removeAttribute('class', 'bg-[#10B981FF]')
+        // console.log(isActive)
     }else{
-       const isActiveDot = document.getElementById('isActiveDot');
-        isActiveDot.classList = ` h-4 w-4 rounded-full bg-[#FF3434]`
-         
+      const isActiveDot = document.getElementById('isActiveDot');
+        isActiveDot.classList.add(`bg-[#FF3434FF]`)
     }
 }
 
 
 
 
+
+let count = 0;
+const buttonHandler = () => {
+ 
+     const markAsRead = document.getElementById('mark-as-read');
+  const createDiv = document.createElement('div');
+  createDiv.innerHTML = `
+  <div class="flex gap-3 rounded-lg p-2 bg-white mt-3">
+  <p class="max-w-64">Sumon</p>
+  <p><i class="fa-regular fa-eye cursor-pointer"></i><span></span></p>
+  </div>
+  ` 
+  markAsRead.appendChild(createDiv)       
+  console.log('clicked')
+
+ count++
+const displayMarkAsReadCount = document.getElementById('mark-as-read-count');
+displayMarkAsReadCount.textContent = `(${count})`;
+
+
+
+
+
+
+
+
+ 
+}
+
+
+
+
 AllPosts()
+
+
+// LatestPosts: - https://openapi.programming-hero.com/api/retro-forum/latest-posts
+
+
+const latestPosts = async() => {
+  const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
+  const data = await res.json();
+  const posts = data;
+  console.log(posts)
+  displayLatestPosts(posts)
+}
+
+
+const displayLatestPosts = (posts) => {
+    posts.forEach(post => {
+
+      const latestPostContainer = document.getElementById('latest-post-container');
+
+      const createLatestPostCard = document.createElement('div');
+      createLatestPostCard.innerHTML = `
+      <div class="border border-bg-[#12132D24] rounded-2xl p-5">
+            <img src="${post.cover_image}" alt="">
+            <div class="max-w-xs h-32 bg-[#12132D96] mx-auto"></div>
+            <p class="text-sm text-[#12132D96]">${post.author.posted_date}</p>
+            <h2 class="text-lg font-bold">${post.title}</h2>
+            <p class="text-[#12132D96] text-m">${post.description
+}</p>
+            <div>
+              <img src="" alt="">
+              <div>
+                <p class="font-semibold">${post.author.name}</p>
+                <p class="text-sm text-[#12132D96]">${post.author?.designation}</p>
+              </div>
+            </div>
+          </div>
+      
+      `
+      latestPostContainer.appendChild(createLatestPostCard)
+    })
+}
+
+latestPosts()
+
+
 
 
 
